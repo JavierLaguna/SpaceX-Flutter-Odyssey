1a3053283a611c8855eb1ff1e495a514ecef8a74
Javier Laguna <javier.laguna92@gmail.com>
2021-01-30 12:31:08 +0100

    Model mapping

--- lib/data/models/launch.dart
+++ lib/data/models/launch.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+
+Launch launchFromMap(String str) => Launch.fromMap(json.decode(str));
 
 class Launch {
   Launch({
@@ -6,20 +9,29 @@ class Launch {
     @required this.missionName,
     @required this.missionID,
     @required this.upcoming,
-    @required this.launchYear,
     @required this.launchDateUnix,
     @required this.launchDateUTC,
     @required this.launchDateLocal,
-    @required this.isTentative,
+    @required this.success,
   });
 
-  int flightNumber;
-  String missionName;
-  List<String> missionID;
-  bool upcoming;
-  String launchYear;
-  int launchDateUnix;
-  String launchDateUTC;
-  DateTime launchDateLocal;
-  bool isTentative;
+  final int flightNumber;
+  final String missionName;
+  final List<String> missionID;
+  final bool upcoming;
+  final int launchDateUnix;
+  final String launchDateUTC;
+  final DateTime launchDateLocal;
+  final bool success;
+
+  factory Launch.fromMap(Map<String, dynamic> json) => Launch(
+        flightNumber: json['flight_number'],
+        missionName: json['name'],
+        missionID: json['id'],
+        upcoming: json['upcoming'],
+        launchDateUnix: json['static_fire_date_unix'],
+        launchDateUTC: json['static_fire_date_utc'],
+        launchDateLocal: json['date_local'],
+        success: json['success'],
+      );
 }
