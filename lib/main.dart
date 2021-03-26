be925e5acccee02e95dcaef0bdbb3fda09176343
Javier Laguna <javier.laguna92@gmail.com>
2021-03-26 18:16:10 +0100

    Fixes translations library

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,11 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await EasyLocalization.ensureInitialized();
+
   runApp(
     EasyLocalization(
         supportedLocales: [Locale('en')],
@@ -28,7 +32,7 @@ class MyApp extends StatelessWidget {
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
-      // DEBUG
+      // Debug things
       enableLog: true,
       debugShowCheckedModeBanner: true,
     );
